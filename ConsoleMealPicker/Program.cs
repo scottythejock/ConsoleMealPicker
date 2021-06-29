fb004acb9f80ba980bbebd91051fe3ad079e4d2d
scottrobertson <scottjrobertson@gmail.com>
2021-06-29 23:29:17 +0100

    - Added a weeks meal plan generator - Currently repeats meals - Removed prompt, generates on launch now

--- ConsoleMealPicker/Program.cs
+++ ConsoleMealPicker/Program.cs
@@ -8,38 +8,47 @@ namespace ConsoleMealPicker
         {
             Console.Title = "M E A L - P I C K E R";
 
-            Console.WriteLine("_________________________");
-            Console.WriteLine("|                       |");
-            Console.WriteLine("| M E A L - P I C K E R |");
-            Console.WriteLine("_________________________");
-
             bool keepGoing = true;
 
             while (keepGoing)
             {
-                string[] myMeal = new string[10];
+                string[] myMeal = new string[12];
                 myMeal[0] = "Burrito";
                 myMeal[1] = "Pizza";
-                myMeal[2] = "Burger";
+                myMeal[2] = "Burger & Wedges";
                 myMeal[3] = "Curry";
                 myMeal[4] = "Baked Potato";
                 myMeal[5] = "Quasedillas";
                 myMeal[6] = "Chilli";
-                myMeal[7] = "Kiev / Pastie";
-                myMeal[8] = "Satay";
-                myMeal[9] = "Macaroni";
+                myMeal[7] = "Kiev or Pastie with Mash & Veg";
+                myMeal[8] = "Chicken Satay Noodles";
+                myMeal[9] = "Macaroni cheese & garlic bread";
+                myMeal[10] = "Takeaway";
+                myMeal[11] = "Pasta Bolognese";
+
+                string[] dayOfWeek = new string[7];
+                dayOfWeek[0] = "Monday";
+                dayOfWeek[1] = "Tuesday";
+                dayOfWeek[2] = "Wednesday";
+                dayOfWeek[3] = "Thursday";
+                dayOfWeek[4] = "Friday";
+                dayOfWeek[5] = "Saturday";
+                dayOfWeek[6] = "Sunday";
 
                 Random random = new Random();
-                int randomNumber = random.Next(0, 9);
-
+                int randomNumber = random.Next(0, 11);
                 string selectedMeal = myMeal[randomNumber];
 
-                Console.WriteLine("Press any key to generate a meal suggestion: ");
+                Console.WriteLine("  /--------------------------/");
+                Console.WriteLine(" /   M E A L - P I C K E R  /");
+                Console.WriteLine("/--------------------------/");
                 Console.WriteLine(" ");
-                Console.ReadKey();
-                Console.WriteLine("Your suggested meal is :");
+                Console.WriteLine("Your suggested meal for today is:");
                 Console.WriteLine("--==[ " + selectedMeal + " ]==--");
                 Console.WriteLine(" ");
+                Console.WriteLine("Your suggested meal plan is as follows :");
+                for (int dayCount = 0, randNum = random.Next(0,11),randMeal = randNum; dayCount < dayOfWeek.Length; dayCount++, randNum = random.Next(0, 9), randMeal = randNum)
+                    Console.WriteLine(dayOfWeek[dayCount] + " - " + myMeal[randMeal]);
                 Console.WriteLine(" ");
                 Console.WriteLine("Continue? Press any key to continue, N or n to exit:");
                 var userWantsToContinue = Console.ReadLine();
